224fafc40df8e57429ac27464e138e519ad1c968
mayunfei <849066462@qq.com>
2013-10-14 22:45:10 +0800

    新建工程
    新建工程

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace WPFBinding
+{
+    public class Person : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void Notify(string propName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        string name;
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.name == value)
+                {
+                    return;
+                }
+                this.name = value;
+                Notify("Name");
+            }
+        }
+
+        int age;
+        public int Age
+        {
+            get { return this.age; }
+            set
+            {
+                if (this.age == value )
+                {
+                    return;
+                }
+                this.age = value;
+                Notify("Age");
+            }
+        }
+
+        public Person() { }
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+    class People : List<Person> { }
+
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.BirthdatyButton.Click += Click;
+        }
+
+        private void Click(object sender, RoutedEventArgs e)
+        {
+            People people = (People)this.FindResource("Family");
+            ICollectionView view =
+                CollectionViewSource.GetDefaultView(people);
+
+            Person person = (Person)view.CurrentItem;
+
+            ++person.Age;
+            MessageBox.Show(
+              string.Format(
+                "Happy Birthday, {0}, age {1}!",
+                person.Name,
+                person.Age),
+              "Birthday");
+        }
+    }
+}
